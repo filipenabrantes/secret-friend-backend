27fb307127a2788fdf3c081b8c6dd411f860044e
Filipe Abrantes <filipenabrantes@gmail.com>
2020-03-13 11:36:38 -0300

    fix

--- src/configs/mail.ts
+++ src/configs/mail.ts
@@ -1,8 +1,8 @@
 class Configs {
-    public host = "smtp.gmail.com";//smtp-relay.sendinblue.com";
+    public host = "smtp-relay.sendinblue.com";
     public port = 587;
     public user = "filipenabrantes@gmail.com";
-    public password = "hk68fgtbfb2c";
+    public password = "WB5Lajhg4qXHnkz9";
 }
 
 export default new Configs;
